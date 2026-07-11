785922b09b9744c98beaa202bd61abb0c34a5636
andy08691 <andy08691@yahoo.com.tw>
2026-07-11 14:20:47 -0400

    feat: add product model

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type Product = {
+  id: string
+  name: string
+  category: string
+  brandModel?: string
+  salePrice: number
+  originalPrice?: number
+  condition?: string
+  defect?: string
+  description?: string
+  status: string
+  quantity: number
+  photos: string[]
+  listedAt?: string
+}
